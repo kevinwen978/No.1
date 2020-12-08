733f3b7cd1fae5bfd78b08069add1b5296a2c7c8
kevin <1390882523@qq.com>
2020-12-08 15:49:04 +0800

    lodash2

--- miao/lodash/kevinwen978-lodash.js
+++ miao/lodash/kevinwen978-lodash.js
@@ -1,9 +1,14 @@
-function compact(ary) {
-    var result = []
-    for (var i = 0;i <  ary.length; i++) {
-        if (ary[i]) {
-            result.push(ary[i])
+var kevinwen978 = function () {
+    function compact(ary) {
+        var result = []
+        for (var i = 0;i < ary.length; i++) {
+            if (ary[i]) {
+                result.push(ary[i])
+            }
         }
+        return result
     }
-    return result
+    return {
+        compact,
+    }
 }
