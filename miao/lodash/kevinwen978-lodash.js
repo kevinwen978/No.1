5adc1d8c38bad2c13dd522af47d7f1c0987eca58
kevin <1390882523@qq.com>
2020-12-08 16:30:57 +0800

    lodash2

--- miao/lodash/kevinwen978-lodash.js
+++ miao/lodash/kevinwen978-lodash.js
@@ -1,4 +1,5 @@
 var kevinwen978 = function () {
+    //过滤数组非真的值
     function compact(ary) {
         var result = []
         for (var i = 0;i < ary.length; i++) {
@@ -8,7 +9,24 @@ var kevinwen978 = function () {
         }
         return result
     }
+    // 将数组分块
+    function chunk(ary, size) {
+        if (ary.length < 1) return []
+        size = size > 0 ? size : 1;
+        let res = [];
+        let l = ary.length;
+        let resL = (l / size) | 0;
+        for (let i = 0; i < resL; i++) {
+          res[i] = ary.slice(size * i, size * (i + 1));
+        }
+        if (resL * size < l) {
+          res[resL] = ary.slice(resL * size);
+        }
+        return res;
+      }                                                                                                                                                                                                                   )
     return {
+        chunk,
         compact,
     }
+
 }();
