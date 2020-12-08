7f0ea58df6e881bc46ae2ec0fb8ed2671e7ca677
kevin <1390882523@qq.com>
2020-12-08 16:44:55 +0800

    lodash3

--- miao/lodash/kevinwen978-lodash.js
+++ miao/lodash/kevinwen978-lodash.js
@@ -12,21 +12,33 @@ var kevinwen978 = function () {
     // 将数组分块
     function chunk(ary, size) {
         if (ary.length < 1) return []
-        size = size > 0 ? size : 1;
-        let res = [];
-        let l = ary.length;
-        let resL = (l / size) | 0;
+        size = size > 0 ? size : 1
+        let res = []
+        let l = ary.length
+        let resL = (l / size) | 0
         for (let i = 0; i < resL; i++) {
           res[i] = ary.slice(size * i, size * (i + 1));
         }
         if (resL * size < l) {
-          res[resL] = ary.slice(resL * size);
+          res[resL] = ary.slice(resL * size)
         }
-        return res;
-      }                                                                                                                                                                                                                   )
+        return res
+    }
+    // 排除数组特定的值  
+    function difference (arr,values) {
+        var result = []
+        for (var i = 0; i < arr.length; i++) {
+            var p = false
+            for (var j = 0; j < values.length; j ++) {
+                if (arr[i] == values[j]) p = true
+            }
+            if (p == false) result.push(arr[i])
+        } return result
+    }                                                                                                                                                                                                                )
     return {
         chunk,
         compact,
+        difference,
     }
 
 }();
