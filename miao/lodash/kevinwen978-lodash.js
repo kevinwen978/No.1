98010a9848a562521261ed7d44466234d562c1ea
kevin <1390882523@qq.com>
2020-12-08 19:20:40 +0800

    lodash3

--- miao/lodash/kevinwen978-lodash.js
+++ miao/lodash/kevinwen978-lodash.js
@@ -25,22 +25,32 @@ var kevinwen978 = function () {
         return result
     }
     // 排除数组特定的值  
-    function difference(ary, values) {
+    function difference(ary, [values]) {
         var result = []
         for (var i = 0; i < ary.length; i++) {
             var p = false
             for (var j = 0; j < values.length; j++) {
-                if (ary[i] == values[j])
+                for (var m = 0; m < values[j].length; m++) {
+                  if (ary[i] == values[j][m])
                     p = true
+                }
             }
             if (p == false)
                 result.push(ary[i])
         } return result
     }
+    //删除数组前面n个元素
+    function drop (arr,n) {
+        var m = arr.length
+        if (n >= m) return []
+        var result = arr.slice(n)
+        return result
+    }
     return {
         chunk,
         compact,
         difference,
+        drop,
     }
 
 } ();
